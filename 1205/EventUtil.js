423ab4c419dcc5c825b3491bd9ea1db5414a70e8
super456 <1061996681@qq.com>
2017-12-07 00:20:16 +0800

    添加、修改浏览器兼容性测试

--- 1205/EventUtil.js
+++ 1205/EventUtil.js
@@ -1,6 +1,8 @@
 //根据浏览器判断事件处理及绑定
 
 var EventUtil={
+
+    //DOM2添加事件处理
     addEventListenerHandler:function(element,type,handle){
         if(element.addEventListener){
             element.addEventListener(type,handle,false);
@@ -10,6 +12,8 @@ var EventUtil={
             element['on'+type]=handle;//直接复制给脚本事件处理
         }
     },
+
+    //DOM2移除事件处理
     removeEventListenerHandler:function(element,type,handle){
         if(element.removeEventListener){
             element.removeEventListener(type,handle,false);
@@ -18,5 +22,28 @@ var EventUtil={
         }else{
             element['on'+type]=null;
         }
+    },
+
+    //判断浏览器兼容性，针对IE浏览器
+    getEvent:function(event){
+        return event?event:window.event;
+    },
+
+    //获取事件对象
+    getTarget:function(event){
+        return event.target||event.srcElement;
+    },
+
+    getPreventDefault:function(event){
+        // if(event.stopPropagation()){
+        //     event.stopPropagation();//阻止冒泡
+        // }else{
+        //      window.event.cancelBubble=true;//IE对象的阻止冒泡
+        //     // event.cancelable=true;
+        //     event.preventDefault();//取消跳转
+        // }
+        event.preventDefault();
     }
+
+
 }
